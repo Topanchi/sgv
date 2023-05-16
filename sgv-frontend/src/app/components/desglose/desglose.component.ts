705485ef4558430be3a0f3048f54a688bb11ecc0
Francisco <franciscofs2000@gmail.com>
2023-05-16 16:52:14 -0400

    maqueta html

--- sgv-frontend/src/app/components/desglose/desglose.component.ts
+++ sgv-frontend/src/app/components/desglose/desglose.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VentaService } from '../../services/venta.service';
 
 @Component({
   selector: 'app-desglose',
@@ -8,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
 export class DesgloseComponent implements OnInit {
 
   public mostrarDatos = null;
+  public mesSeleccionado: number;
+  public anioSeleccionado: number;
 
-  constructor() { }
+  public anio: any = {
+    idanio: ''
+  };
+  public mes: any = {
+    idmes: ''
+  };
+  public years: number[] = [];
+
+  constructor( private _ventaService: VentaService) { }
 
   ngOnInit(): void {
+    this.obtenerAnios();
+  }
+
+  public getDataAnio(id:any) {
+    console.log("Año seleccionado: ", +id.value);
+    console.log("Año seleccionado: ", typeof +id.value);
+    this.anioSeleccionado = +id.value;
+  }
+
+  public getDataMes(id:any) {
+    console.log("Mes seleccionado: ", +id.value);
+    console.log("Mes seleccionado: ", typeof +id.value);
+    this.mesSeleccionado = +id.value;
+
+    console.log("año: ", this.anioSeleccionado, "mes: ", this.mesSeleccionado);
+    //Llamar al servicio y llenar data
+  }
+
+  private obtenerAnios() {
+    const currentYear = new Date().getFullYear();
+    const startYear = currentYear - 5; // Rango de años que deseas mostrar (10 años en este ejemplo)
+
+    for (let i = startYear; i <= currentYear; i++) {
+      this.years.push(i);
+    }
   }
 
 }
